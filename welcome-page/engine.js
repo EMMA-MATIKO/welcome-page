fc754d70a4140eef36c2058815d851765e471ccc
Emmanuel Matiko <emmanuel.kichuki@gmail.com>
2024-06-24 13:40:26 +0300

    Update engine.js

--- welcome-page/engine.js
+++ welcome-page/engine.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const textElement = document.getElementById("typing-text");
-    const text = "Let's explore the web, privately and securely 😉";
+    const text = "Let's explore the web fast, privately and securely 😉";
     let index = 0;
 
     function type() {
@@ -18,4 +18,28 @@ document.addEventListener("DOMContentLoaded", function () {
         textElement.classList.add("typing-effect");
         type();
     });
+
+    function updateTimeAndGreeting() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds()
+
+        const formattedTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+        document.getElementById('time').textContent = `Current time: ${formattedTime}`;
+
+        let greeting = '';
+        if (hours < 12) {
+            greeting = 'Good morning boss!';
+        } else if (hours < 18) {
+            greeting = 'Good afternoon boss!';
+        } else {
+            greeting = 'Good evening boss!';
+        }
+        document.getElementById('greeting').textContent = greeting;
+
+        setTimeout(updateTimeAndGreeting, 1000);
+    }
+
+    updateTimeAndGreeting();
 });
